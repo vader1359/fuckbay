74397c5c7230cea48dfed034e3611c7029cf12bc
vader1359 <vader1359@gmail.com>
2017-08-16 16:55:43 +0700

    Add partial post and render all in main screen

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170816082010) do
+ActiveRecord::Schema.define(version: 20170816094200) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.integer "author_id"
+    t.string "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
