24fc88d0fd512f4f7e3ef6f0ca46fa52f45351b9
vader1359 <vader1359@gmail.com>
2017-08-27 09:24:50 +0700

    Before rotate scrolling

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,7 +2,7 @@ class MessagesController < ApplicationController
   def index
     @friend_list_img = list_friendship_img(current_user)
 
-    @chatting_friends = [User.third, User.second]
+    @chatting_friends = [User.third, User.second, User.all[4], User.all[5], User.all[3]]
     
   end
 
