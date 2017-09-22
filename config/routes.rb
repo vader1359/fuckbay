41955c424eff2d1a8e45baf8fc294cc62b4eea87
vader1359 <vader1359@gmail.com>
2017-09-22 11:04:59 +0700

    Beyond the sea

--- config/routes.rb
+++ config/routes.rb
@@ -42,5 +42,5 @@ Rails.application.routes.draw do
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-  root "users#index" # This lead to the timeline page
+  root "messages#index" # This lead to the timeline page
 end
